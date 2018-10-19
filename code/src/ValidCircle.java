a2d7938cafa3c28ba4540422f923deabe583b924
pengy <1013799318@qq.com>
2018-10-19 22:09:56 +0800

    out of range
    out of range

--- code/src/ValidCircle.java
+++ code/src/ValidCircle.java
@@ -29,6 +29,7 @@ public class ValidCircle {
         double e = ((x1 * x1 - x2 * x2) + (y1 * y1 - y2 * y2)) / 2.0;
         double f = ((x1 * x1 - x3 * x3) + (y1 * y1 - y3 * y3)) / 2.0;
         double det = b * c - a * d;
+        if (det==0) return -1;
         double x0 = -(d * e - b * f) / det;
         double y0 = -(a * f - c * e) / det;
         System.out.println("("+x0+","+y0+")");
